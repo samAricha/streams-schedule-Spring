d5a644f31c36b667da3da8762a6419d2bf27c48e
samAricha <samaricha001@gmail.com>
2023-02-19 23:58:55 +0300

    controller setup complete

--- src/main/java/com/example/streams_schedule/controller/LiveStreamController.java
+++ src/main/java/com/example/streams_schedule/controller/LiveStreamController.java
@@ -2,9 +2,9 @@ package com.example.streams_schedule.controller;
 
 import com.example.streams_schedule.model.LiveStream;
 import com.example.streams_schedule.repository.LiveStreamRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +13,46 @@ import java.util.List;
 @RequestMapping("/streams")
 public class LiveStreamController {
 
+    private final LiveStreamRepository repository;
 
+    public LiveStreamController(LiveStreamRepository repository){
+        this.repository = repository;
+    }
     //http://localhost:8080/streams
     @GetMapping
     public List<LiveStream> findAll(){
-        return null;
+        return repository.findAll();
     }
+
+    //GET http://localhost:8080/streams/1234-2345-3456-8798
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id){
+        return repository.findById(id);
+    }
+
+    //POST http://localhost:8080/streams/
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@Valid @RequestBody LiveStream stream){//using jackson for the mapping of the body
+       return repository.create(stream);
+    }
+
+    //PUT http://localhost:8080/streams/
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody LiveStream stream, @PathVariable String id){
+
+        repository.update(stream, id);
+
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id){
+
+        repository.delete(id);
+
+    }
+
+
 }
--- src/main/java/com/example/streams_schedule/model/LiveStream.java
+++ src/main/java/com/example/streams_schedule/model/LiveStream.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 public record  LiveStream(
         String id,
-        @NotEmpty(message = "Stream title is required")
+        @NotEmpty(message = "Stream title is required")//validation annotation
         String title,
         String description,
         String url,
